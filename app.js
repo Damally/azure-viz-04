5e0c8504b090ff700048bfdec9be31b7bf7d3dc7
Dr. Neil Brittliff <Neil.Brittliff@homemail.com.au>
2018-09-12 11:17:13 +1000

    Updated to new version

--- app.js
+++ app.js
@@ -8,25 +8,31 @@ var routes = require('./routes');
 var http = require('http');
 var path = require('path');
 var pug = require('pug');
+var logger = require('morgan');
+var favicon = require('serve-favicon');
+var methodOverride = require('method-override');
+var bodyParser = require('body-parser');
+var errorHandler = require('errorhandler');
 
 var app = express();
+app.use(favicon(path.join(__dirname, 'public','images','favicon.ico'))); 
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
-app.use("/modules", express.static(path.join(__dirname, 'node_modules')));
+app.use(logger('dev'));
+app.use(methodOverride());
+app.use(express.json());    
 
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+if (process.env.NODE_ENV === 'development') {
+   app.use(errorhandler())
 }
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use("/modules", express.static(path.join(__dirname, 'node_modules')));
 
 function logMessage(message) {
   
